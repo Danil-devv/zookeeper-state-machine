2194470e78ad3735649c92ca8918a9a39393d030
danil <s3menovd4nil@yandex.ru>
2024-04-11 22:37:38 +0300

    feat: add graceful shutdown

--- cmd/election/main.go
+++ cmd/election/main.go
@@ -3,21 +3,36 @@ package main
 import (
 	"context"
 	"fmt"
+	"golang.org/x/sync/errgroup"
 	"os"
+	"os/signal"
 
 	"github.com/central-university-dev/2024-spring-go-course-lesson8-leader-election/internal/commands"
 )
 
 func main() {
+	eg, _ := errgroup.WithContext(context.Background())
 	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
+
+	errCh := make(chan os.Signal, 1)
+	signal.Notify(errCh, os.Interrupt)
+	go func() {
+		<-errCh
+		fmt.Println("got SIGINT notify")
+		cancel()
+	}()
+
 	rootCmd, err := commands.InitRunCommand(ctx)
 	if err != nil {
 		fmt.Println("init run command: %w", err)
 		os.Exit(1)
 	}
-	err = rootCmd.Execute()
-	if err != nil {
+
+	eg.Go(func() error {
+		return rootCmd.Execute()
+	})
+
+	if err = eg.Wait(); err != nil {
 		fmt.Println("run command: %w", err)
 		os.Exit(1)
 	}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-zookeeper/zk v1.0.3
 	github.com/google/uuid v1.6.0
 	github.com/spf13/cobra v1.8.0
+	golang.org/x/sync v0.7.0
 )
 
 require (
