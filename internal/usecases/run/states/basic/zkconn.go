fdaccf11072d1224862c95c29ac555e78d09e3e6
danil <s3menovd4nil@yandex.ru>
2024-04-14 03:37:32 +0300

    tests: add mocks

--- internal/usecases/run/states/basic/zkconn.go
+++ internal/usecases/run/states/basic/zkconn.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+//go:generate go run github.com/vektra/mockery/v2@v2.42.2 --output=./states/mocks --name=ZkConn
 type ZkConn interface {
 	Create(path string, data []byte, flags int32, acl []zk.ACL) (string, error)
 	Delete(path string, version int32) error
--- internal/usecases/run/states/state.go
+++ internal/usecases/run/states/state.go
@@ -5,6 +5,7 @@ import (
 	"hw/internal/usecases/run/states/basic"
 )
 
+//go:generate go run github.com/vektra/mockery/v2@v2.42.2 --output=./mocks --name=AutomataState
 type AutomataState interface {
 	Run(ctx context.Context) (basic.StateID, error)
 	String() string
